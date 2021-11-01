7882b7fa260b605685ca2eeceb629f32b5b0dacd
Marco <52253797+quartzexpressDEV@users.noreply.github.com>
2021-11-01 18:47:27 +0100

    what

--- edit.js
+++ edit.js
@@ -1,8 +1,8 @@
-browser.storage.sync.get("old").then(function OnGot(item){
-    if(item.old.c){
+chrome.storage.sync.get(["old"], function(result){
+    if(result.old){
         document.getElementsByClassName("block cc_discordWidget")[0].getElementsByClassName("block-container")[0].style.backgroundColor = "#738BD7";
 }
 else{
     document.getElementsByClassName("block cc_discordWidget")[0].getElementsByClassName("block-container")[0].style.backgroundColor = "#5865F2";
 }
-}, function OnError(error){console.log(`Error: ${error}`);});
+})
