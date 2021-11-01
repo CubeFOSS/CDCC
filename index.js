cfd59a2cc14cc8aa500fbe767014704e30d944a0
Marco <52253797+quartzexpressDEV@users.noreply.github.com>
2021-11-01 18:10:26 +0100

    fixed settings not saving

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function save(){
     let oldc = document.getElementById("classic").checked;
     browser.storage.sync.set({
-        old: oldc
-    }, function(){
-        let status = document.getElementById("status");
-        status.textContent = "Saved!";
-        setTimeout(function(){
-            status.textContent = "";
-        }, 2000);
+        old: {c:oldc}
     });
+    console.log(oldc);
+      let status = document.getElementById("status");
+      status.textContent = "Saved!";
+      setTimeout(function(){
+          status.textContent = "";
+      }, 2000);
 }
 function restore() {
-  browser.storage.local.get("old").then(function OnGot(item){document.getElementById("classic").checked = item;}, function OnError(error){console.log(`Error: ${error}`);})
+  let a = browser.storage.sync.get("old");
+  a.then(onGot, onError)
+}
+function onGot(item){
+  document.getElementById("classic").checked = item.old.c;
+  console.log(item.old.c)
+}
+function onError(error){
+  console.log(`Error: ${error}`);
 }
 document.addEventListener('DOMContentLoaded', restore);
 document.getElementById('save').addEventListener('click',
